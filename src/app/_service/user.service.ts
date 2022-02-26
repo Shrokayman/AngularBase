e1d7e61d997fc49724fa7778d777a04be2ace629
Shrokayman <shrokayman99@gmail.com>
2022-02-26 18:00:41 +0200

    Implementing show and update with Api

--- src/app/_service/user.service.ts
+++ src/app/_service/user.service.ts
@@ -10,10 +10,16 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   registerUser(data: any){
-    return this.http.post(environment.apiUrl+'/api/register/' , data);
+    return this.http.post(environment.apiUrl+'/register/' , data);
   }
 
   login(data: any){
-    return this.http.post(environment.apiUrl+'/api/login/' , data)
+    return this.http.post(environment.apiUrl+'/login/' , data)
+  }
+
+  
+
+  updateUser(data:any , id : number){
+    return this.http.put(environment.apiUrl+'/update/'+id ,data);
   }
 }
