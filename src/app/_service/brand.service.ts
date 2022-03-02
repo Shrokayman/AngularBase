ecdbf115ed28b11d101e58f75aa26143e58afede
MahmoudLebda1997 <mahmodsaadlebda@gmail.com>
2022-03-02 10:30:40 +0200

    Adding Product Category Brand Protection

--- src/app/_service/brand.service.ts
+++ src/app/_service/brand.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Brand } from '../_models/brand';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,24 @@ export class BrandService {
 
   constructor(private http: HttpClient) { }
   getBrandData(){
+    // let header = new HttpHeaders({
+    //   Authorization: localStorage.getItem('token')!
+    // })
+    // return this.http.get(environment.apiUrl+'/brands', {headers:header});
     return this.http.get('http://127.0.0.1:8000/api/brands')
   }
   addData(data:Brand){
+        // let header= new HttpHeaders({
+    //   Authorization: localStorage.getItem('token')!
+    // })
+    // return this.http.post(environment.apiUrl+'/categories', {headers:header}, data);
     return this.http.post('http://127.0.0.1:8000/api/brands',data);
   }
   deleteData(id:any){
-    return this.http.delete('http://127.0.0.1:8000/api/brands/'+id);
+    // return this.http.delete('http://127.0.0.1:8000/api/brands/'+id);
+    let header = new HttpHeaders({
+      Authorization: localStorage.getItem('token')!
+    })
+    return this.http.delete(environment.apiUrl+'/brands/'+id, {headers:header});
   }
-}
+  }
--- src/app/_service/category.service.ts
+++ src/app/_service/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Category } from '../_models/category';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,24 @@ export class CategoryService {
   constructor(private http:HttpClient) { }
   getData(){
     return this.http.get('http://127.0.0.1:8000/api/categories')
+    // let header = new HttpHeaders({
+    //   Authorization: localStorage.getItem('token')!
+    // })
+    // return this.http.get(environment.apiUrl+'/categories', {headers:header});
   }
   addData(data:Category){
+    // let header= new HttpHeaders({
+    //   Authorization: localStorage.getItem('token')!
+    // })
+    // return this.http.post(environment.apiUrl+'/categories', {headers:header}, data);
     return this.http.post('http://127.0.0.1:8000/api/categories',data);
   }
   deleteData(id:any){
-    return this.http.delete('http://127.0.0.1:8000/api/categories/'+id);
+    // return this.http.delete('http://127.0.0.1:8000/api/categories/'+id);
+    let header = new HttpHeaders({
+      Authorization: localStorage.getItem('token')!
+    })
+    return this.http.delete(environment.apiUrl+'/categories/'+id, {headers:header});
   }
-}
+  }
+
