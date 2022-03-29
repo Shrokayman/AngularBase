44d165853454e0f707b39caa59bee37275b2d7e7
Mahmooud94 <mahmoud_gamal94@hotmail.com>
2022-03-30 00:32:36 +0200

    finished

--- src/app/core/cart/checkout/checkout.component.ts
+++ src/app/core/cart/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import { CartService } from 'src/app/_service/cart.service';
 import { OrderService } from 'src/app/_service/order.service';
 
 
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -11,42 +12,48 @@ import { OrderService } from 'src/app/_service/order.service';
 })
 export class CheckoutComponent implements OnInit {
 
-  imageDirectoryPath: any = 'http://127.0.0.1:8000/storage/products/';
-  token: any = localStorage.getItem('token');
-  userData: any;
-  id: any;
-  cart: any;
 
-  constructor(private cartService: CartService, private orderService: OrderService) { }
 
-  ngOnInit(): void {
-    this.userData = localStorage.getItem('token');
-    this.userData = jwt_decode(this.token);
-    this.id = this.userData.user_id;
-    this.getCart();
-    this.cartService.cartRecordList.subscribe((res: any) => {
-      this.cart = res;
-    })
-  }
+imageDirectoryPath: any = 'http://127.0.0.1:8000/storage/products/';
+token: any = localStorage.getItem('token');
+userData: any;
+id: any;
+cart: any;
 
-  getCart() {
-    this.cartService.getCart(this.id).subscribe((res: any) => {
-      this.cartService.cartRecordList.emit(res);
-      // this.cart.products.forEach((item: any) => {
-      //   let itemPrice;
-      //   itemPrice = item.price * item.pivot.product_quantity;
-      //   this.totalPrice += itemPrice;
-      //   return this.totalPrice
-      // })
-    });
-  }
+constructor(private cartService: CartService, private orderService: OrderService) { }
 
-  createOrder () {
-    this.orderService.createOrder(this.cart).subscribe(()=>{})
-    this.cartService.deleteCart(this.id).subscribe(() => {
-      this.cartService.cartHasBeenChanged.emit([]);
-      this.cart.products = [];
-    });
-  }
+ngOnInit(): void {
+
+  this.userData = localStorage.getItem('token');
+  this.userData = jwt_decode(this.token);
+  this.id = this.userData.user_id;
+
+  this.getCart();
+  this.cartService.cartRecordList.subscribe((res: any) => {
+    this.cart = res;
+  })
+  
+}
+
+getCart() {
+  this.cartService.getCart(this.id).subscribe((res: any) => {
+    this.cartService.cartRecordList.emit(res);
+    // this.cart.products.forEach((item: any) => {
+    //   let itemPrice;
+    //   itemPrice = item.price * item.pivot.product_quantity;
+    //   this.totalPrice += itemPrice;
+    //   return this.totalPrice
+    // })
+  });
+}
+
+createOrder() {
+  this.orderService.createOrder(this.cart).subscribe(() => { })
+  this.cartService.deleteCart(this.id).subscribe(() => {
+    this.cartService.cartHasBeenChanged.emit([]);
+    this.cart.products = [];
+  });
+}
+
 
 }
